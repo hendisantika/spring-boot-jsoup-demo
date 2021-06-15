cc6e1b766c43c7a8cc3dd8f5669ced8d5a4eeb07
Hendi Santika <hendisantika@gmail.com>
2021-06-15 07:59:36 +0700

    #12 - Add GlobalExceptionHandler

---
+++ src/main/java/com/hendisantika/jsoupdemo/config/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.hendisantika.jsoupdemo.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : jsoup-demo
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 15/06/21
+ * Time: 07.59
+ */
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    private static final Logger LOG = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+}
