8bed91f77b0bd3fbc696123545ddeba4117d0527
Hendi Santika <hendisantika@gmail.com>
2021-06-15 08:00:00 +0700

    #13 - Add handle in GlobalExceptionHandler

--- src/main/java/com/hendisantika/jsoupdemo/config/GlobalExceptionHandler.java
+++ src/main/java/com/hendisantika/jsoupdemo/config/GlobalExceptionHandler.java
@@ -2,7 +2,14 @@ package com.hendisantika.jsoupdemo.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.Map;
 
 /**
  * Created by IntelliJ IDEA.
@@ -18,4 +25,11 @@ public class GlobalExceptionHandler {
 
     private static final Logger LOG = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
+    @ExceptionHandler
+    @ResponseBody
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map handle(MissingServletRequestParameterException exception) {
+        LOG.error(exception.getMessage(), exception);
+        return error(exception.getMessage());
+    }
 }
