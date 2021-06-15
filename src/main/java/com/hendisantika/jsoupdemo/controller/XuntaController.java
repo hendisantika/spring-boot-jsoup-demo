95465887b40240f87cbde7f336214064e2090c8c
Hendi Santika <hendisantika@gmail.com>
2021-06-15 08:05:12 +0700

    #20 - Add getCommissions in XuntaController

--- src/main/java/com/hendisantika/jsoupdemo/controller/XuntaController.java
+++ src/main/java/com/hendisantika/jsoupdemo/controller/XuntaController.java
@@ -1,12 +1,25 @@
 package com.hendisantika.jsoupdemo.controller;
 
+import com.hendisantika.jsoupdemo.model.CommissionCriteria;
+import com.hendisantika.jsoupdemo.model.CommissionResult;
 import com.hendisantika.jsoupdemo.service.XuntaService;
 import lombok.extern.log4j.Log4j2;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import javax.validation.Valid;
+import javax.validation.constraints.Email;
+import java.io.IOException;
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -27,4 +40,21 @@ public class XuntaController {
 
     @Autowired
     private JavaMailSender javaMailSender;
+
+    @GetMapping("commission")
+    public List<CommissionResult> getCommissions(@Email String recipient, @Valid CommissionCriteria filters)
+            throws IOException, MessagingException {
+
+        log.info("request started -> '/xunta/commission' -> recipient ({}), filters ({})", recipient, filters);
+        final List<CommissionResult> commissions = xuntaService.getCommissions(filters);
+        if (StringUtils.isNotEmpty(recipient) && commissions.size() > 0) {
+            MimeMessage msg = javaMailSender.createMimeMessage();
+            msg.setRecipient(Message.RecipientType.TO, new InternetAddress(recipient));
+            msg.setSubject("Xunta's Commission Results");
+            msg.setContent(xuntaService.createCommissionEmailContent(filters, commissions), "text/html;charset=UTF-8");
+            javaMailSender.send(msg);
+        }
+        log.info("request finished -> results ({})", commissions.size());
+        return commissions;
+    }
 }
